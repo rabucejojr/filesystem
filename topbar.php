aa3cefec2147041fcf02e3d4f027a3862b64ad82
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-01-22 14:32:41 +0800

    change color

--- topbar.php
+++ topbar.php
@@ -9,14 +9,14 @@
 }
 </style>
 
-<nav class="navbar navbar-dark bg-success fixed-top " style="padding:0;">
+<nav class="navbar navbar-dark bg-white fixed-top " style="padding:0;">
   <div class="container-fluid mt-2 mb-2">
   	<div class="col-lg-12">
   		<div class="col-md-1 float-left" style="display: flex;">
   			<img width="40" src="assets/dost_logo.png">
   		</div>
 	  	<div class="col-md-2 mt-2 float-right">
-	  		<a class="text-light" href="ajax.php?action=logout"><?php echo $_SESSION['login_name'] ?> <i class="fa fa-power-off"></i></a>
+	  		<a class="text-dark" href="ajax.php?action=logout"><?php echo $_SESSION['login_name'] ?> <i class="fa fa-power-off"></i></a>
 	    </div>
     </div>
   </div>
