452ee9f63fc1712a58254c9730621079cda90d6d
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-01-23 12:15:01 +0800

    refractor

--- login.php
+++ login.php
@@ -2,122 +2,128 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>PSTO:FMS</title>
- 	
+	<title>PSTO:FMS</title>
 
-<?php include('./header.php'); ?>
-<?php 
-session_start();
-if(isset($_SESSION['login_id']))
-header("location:index.php?page=home");
-?>
+
+	<?php include('./header.php'); ?>
+	<?php
+	session_start();
+	if (isset($_SESSION['login_id']))
+		header("location:index.php?page=home");
+	?>
 
 </head>
 <style>
-	body{
+	body {
 		width: 100%;
-	    height: calc(100%);
-	    /*background: #007bff;*/
-	}
-	main#main{
-		width:100%;
 		height: calc(100%);
-		background:white;
+		/*background: #007bff;*/
 	}
-	#login-right{
+
+	main#main {
+		width: 100%;
+		height: calc(100%);
+		background: white;
+	}
+
+	#login-right {
 		position: absolute;
-		right:0;
-		width:50%;
+		right: 0;
+		width: 50%;
 		height: calc(100%);
-		background:white;
+		background: white;
 		display: flex;
 		align-items: center;
 	}
-	#login-left{
+
+	#login-left {
 		position: absolute;
-		left:0;
-		width:50%;
+		left: 0;
+		width: 50%;
 		height: calc(100%);
-		background:white;
+		background: white;
 		display: flex;
 		align-items: center;
 	}
-	#login-right .card{
+
+	#login-right .card {
 		margin: auto
 	}
+
 	.logo {
-    margin: auto;
-    font-size: 8rem;
-    padding: .5em 0.8em;
-    color: #000000b3;
-}
+		margin: auto;
+		font-size: 8rem;
+		padding: .5em 0.8em;
+		color: #000000b3;
+	}
 </style>
 
 <body>
 
 
-  <main id="main" class=" alert-info">
-  		<div id="login-left">
-  			<div class="logo">
-  				<img src="assets/dost_logo.png">
-  			</div>
-  		</div>
-  		<div id="login-right">
-  			<div class="w-100">
-  				<h4 class="text-primary text-center"><b>PSTO-SDN <br>File Management System</br></h4>
-  				<br>
-  			
-  			<div class="card col-md-6">
-  				<div class="card-body">
-  					<form id="login-form" >
-  						<div class="form-group">
-  							<label for="username" class="control-label text-primary">Username</label>
-  							<input type="text" id="username" name="username" class="form-control">
-  						</div>
-  						<div class="form-group">
-  							<label for="password" class="control-label text-primary">Password</label>
-  							<input type="password" id="password" name="password" class="form-control">
-  						</div>
-  						<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
-  					</form>
-  				</div>
-  			</div>
-  		</div>
-   		</div>
+	<main id="main" class=" alert-info">
+		<div id="login-left">
+			<div class="logo">
+				<img src="assets/dost_logo.png">
+			</div>
+		</div>
+		<div id="login-right">
+			<div class="w-100">
+				<h4 class="text-primary text-center"><b>PSTO-SDN <br>File Management System</br></h4>
+				<br>
 
-  </main>
+				<div class="card col-md-6">
+					<div class="card-body">
+						<form id="login-form">
+							<div class="form-group">
+								<label for="username" class="control-label text-primary">Username</label>
+								<input type="text" id="username" name="username" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="password" class="control-label text-primary">Password</label>
+								<input type="password" id="password" name="password" class="form-control">
+							</div>
+							<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 
-  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+	</main>
+
+	<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
 
 </body>
 <script>
-	$('#login-form').submit(function(e){
+	$('#login-form').submit(function(e) {
 		e.preventDefault()
-		$('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
-		if($(this).find('.alert-danger').length > 0 )
+		$('#login-form button[type="button"]').attr('disabled', true).html('Logging in...');
+		if ($(this).find('.alert-danger').length > 0)
 			$(this).find('.alert-danger').remove();
 		$.ajax({
-			url:'ajax.php?action=login',
-			method:'POST',
-			data:$(this).serialize(),
-			error:err=>{
+			url: 'ajax.php?action=login',
+			method: 'POST',
+			data: $(this).serialize(),
+			error: err => {
 				console.log(err)
-		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+				$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
 
 			},
-			success:function(resp){
-				if(resp == 1){
+			success: function(resp) {
+				if (resp == 1) {
 					location.reload('index.php?page=home');
-				}else{
+				} else {
 					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
 					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
 				}
 			}
 		})
 	})
-</script>	
+</script>
+
 </html>
